03e6a5a0188973b5db6d051ed05469ec13e533b6
Xfirepc <vinygfx@gmail.com>
2018-06-26 23:03:41 -0500

    add Sinon for passed funcions on ava's tests

--- platziverse-db/tests/agent-test.js
+++ platziverse-db/tests/agent-test.js
@@ -1,14 +1,39 @@
 'use strict'
 
 const test = require('ava')
+const sinon = require('sinon')
+const proxyquire = require('proxyquire')
 
 let db = null
+
 let config = {
   logging: function() {}
 }
+
+let MetricStub = {
+  belongsTo: sinon.spy()
+}
+
+let AgentStub = null
+let sandbox = null
+
 test.beforeEach(async () => {
-  const setupDatabase = require('../')
+  sandobox = sinon.sandbox.create()
+
+  AgentStub = {
+    hasMany: sandbox.spy()
+  }
+
+  const setupDatabase = proxyquire('../', {
+    './models/agent': () => AgentStub,
+    './models/metric': () => MetricStub
+
+  })
   db = await setupDatabase(config)
+})
+
+test.afterEach(() => {
+  sandbox && sinon.sandbox.restore()
 })
 
 test('Agent', t => {
