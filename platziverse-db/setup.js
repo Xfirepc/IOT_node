3ece2ebbc9cb3f9dddc2364c66b3fe8e423bf454
Xfirepc <vinygfx@gmail.com>
2018-06-26 19:48:43 -0500

    Ading unit test wiht Ava Js

--- platziverse-db/setup.js
+++ platziverse-db/setup.js
@@ -1,9 +1,25 @@
 'use strict'
 
 const debug = require('debug')('platziverse:db:setup')
+const inquirer = require('inquirer')
+const chalk = require('chalk')
 const db = require('./')
 
+const prompt = inquirer.createPromptModule()
+
 async function setup () {
+
+  const answer = await prompt([{
+
+    type: 'confirm',
+    name: 'setup',
+    message: 'This will destroy database, Are you sure'
+
+  }])
+
+  if(!answer.setup)
+    return console.log('Nothing happend!  :) ')
+
   const config = {
     database: process.env.DB_NAME || 'platziverse',
     username: process.env.DB_USER || 'platzi',
@@ -20,7 +36,7 @@ async function setup () {
 }
 
 function handleFatalError (err) {
-  console.error(err.message)
+  console.error(`${chalk.red('[ FATAL ERROR ]:  ')} ${err.message}`)
   console.error(err.stack)
   process.exit(1)
 }
