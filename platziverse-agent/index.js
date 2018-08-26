7698663ec7f5ea0c4e996ce55b3e372a756df497
Xfirepc <vinygfx@gmail.com>
2018-08-26 17:38:35 -0500

    Adding class EventEmmiter to emit and recive topics

--- platziverse-agent/index.js
+++ platziverse-agent/index.js
@@ -1,26 +1,70 @@
 'use strict'
 
+const debug = require('debug')
+const mqtt = require('mqtt')
+const defaults = require('defaults')
 const EventEmmiter = require('events')
+const { parsePayload } = require('../platziverse-mqtt/utils.js')
+const uuid = require('uuid')
 
+const options = {
+  name: 'untitled',
+  username: 'platzi',
+  inteval: 5000,
+  mqtt: {
+    host: 'mqtt://localhost'
+  }
+}
 class PlatziverseAgent extends EventEmmiter{
   constructor(opts) {
     super()
 
-    this._options = opts
+    this._options = defaults(opts, options)
     this._timer = null
     this._started = false
+    this._client = null
+    this._agentId = null
   }
 
   connect() {
     if(!this._started){
-      this._started = true
-      this.emit('connected')
       const opts = this._options
+      this._client = mqtt.connect(opts.mqtt.host)
+      this._started = true
 
-      this._timer = setInterval(() => {
-        this.emit('agent/message', 'this is a message')
-      }, opts.interval)
+      this._client.subscribe('agent/message')
+      this._client.subscribe('agent/connected')
+      this._client.subscribe('agent/disconnected')
 
+      this._client.on('connected', ()=> {
+        this._agentId = uuid.v4()
+
+        this.emit('connected', this._agentId)
+
+        this._timer = setInterval(() => {
+          this.emit('agent/message', 'this is a message')
+        }, opts.interval)
+      })
+
+      this._client.on('message', (topic, payload) => {
+        payload = parsePayload(payload)
+
+        let broadcast = false
+        switch (topic) {
+          case 'agent/connected':
+          case 'agent/disconnected':
+          case 'agent/message':
+            broadcast = payload && payload.agent && payload.agent.uuid
+            beak
+        }
+
+        if (broadcast) {
+          this.emit(topic, payload)
+        }
+      })
+
+      this._client.on('error', ()=> this.disconnect())
+      
     }
   }
 
