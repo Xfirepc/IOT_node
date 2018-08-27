7bcc26b7f4589e3cef51a6a36ad301dd88269b9d
Xfirepc <vinygfx@gmail.com>
2018-08-27 18:15:59 -0500

    add server and modules to generate app web

---
+++ platziverse-api/server.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const http = require('http')
+const express = require('express')
+const chalk = require('chalk')
+const port = process.env.PORT || 3000
+const app = express()
+const server = http.createServer(app)
+
+server.listen(port, ()=>{
+  console.log(`${chalk.green('[platziverse-api]')} server runng on port: ${port}`)
+})
