e0c239e66e2512b3dfd3d5ebb783fc146e395011
Xfirepc <vinygfx@gmail.com>
2018-08-30 15:36:08 -0500

    adding api's returns routes

--- platziverse-api/api.js
+++ platziverse-api/api.js
@@ -11,6 +11,7 @@ let services, Agent, Metric
 api.use('*', async (req, res, next) => {
   if (!services) {
     debug('Connecting to database..')
+
     try {
       services = await db(configDB)
     } catch (e) {
@@ -20,47 +21,78 @@ api.use('*', async (req, res, next) => {
     Agent = services.Agent
     Metric = services.Metric
   }
+
   next()
 })
 
-
 api.get('/agents', async (req, res, next) => {
   debug('A request has to come /agents')
+
   let agents = []
+
   try {
     agents = await Agent.findConnected()
   } catch (e) {
     return next(e)
   }
+
   res.send({agents})
 })
-
 
 api.get('/agent/:uuid', async (req, res, next) => {
   const { uuid } = req.params
   debug('A request has to come /agent/:uuid')
+
   let agent
+
   try {
     agent = await Agent.findByUuid(uuid)
   } catch (e) {
     return next(e)
   }
+
   if (!agent) {
     return next(new Error(`Agent with uuid ${uuid} not found`))
   }
+
   res.send({ agent })
 })
 
-
-
-
-api.get('/agent/:metrics', (req, res) => {
+api.get('/metrics/:uuid', async (req, res, next) => {
   const { uuid } = req.params
-  res.send({ uuid })
+  debug('A request has to come /metrics/:uuid')
+
+  let metrics = []
+
+  try {
+    metrics = await Metric.findByAgentUuid(uuid)
+  } catch (e) {
+    return next(e)
+  }
+
+  if (!metrics || metrics.length === 0) {
+    return next(new Error(`Metrics not found for agent ID: ${uuid}`))
+  }
+  res.send({ metrics })
 })
 
-api.get('/agent/:uuid/:type', (req, res) => {
+api.get('/metrics/:uuid/:type', async (req, res, next) => {
   const { uuid, type } = req.params
-  res.send({ uuid, type })
+
+  debug('A request has to come /metrics/:uuid/:type')
+
+  let metrics = []
+
+  try {
+    metrics = await Metric.findByTypeAgentUuid(type, uuid)
+  } catch (e) {
+    return next(e)
+  }
+
+  if (!metrics || metrics.length === 0) {
+    return next(new Error(`Metrics not found for agent ID: ${uuid} and type: ${type}`))
+  }
+
+  res.send({ metrics })
 })
 module.exports = api
