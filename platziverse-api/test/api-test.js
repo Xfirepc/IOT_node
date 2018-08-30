aa5e4948634e05616df9f389c40acd2fba666909
Xfirepc <vinygfx@gmail.com>
2018-08-30 16:33:23 -0500

    implemente test to api

--- platziverse-api/test/api-test.js
+++ platziverse-api/test/api-test.js
@@ -2,16 +2,56 @@
 
 const test = require('ava')
 const request = require('supertest')
+const sinon = require('sinon')
+const proxyquire = require('proxyquire')
+const agentFixtures = require('../../platziverse-db/tests/fixtures/agent')
 
-const server = require('../server')
+let sandbox = null
+let server = null
+let dbStub = null
+let AgentStub = {}
+let MetricStub = {}
+
+test.beforeEach(async () => {
+  sandbox = sinon.createSandbox()
+
+  dbStub = sandbox.stub()
+  dbStub.returns(Promise.resolve({
+    Agent: AgentStub,
+    Metric: MetricStub
+  }))
+
+  AgentStub.findConnected = sandbox.stub()
+  AgentStub.findConnected.returns(Promise.resolve(agentFixtures.connected))
+
+  const api = proxyquire('../api', {
+    'platziverse-db': dbStub
+  })
+
+  server = proxyquire('../server', {
+    './api': api
+  })
+
+})
+
+test.afterEach(() => {
+  sandbox = sinon.reset()
+})
 
 test.serial.cb('/api/agents', t => {
   request(server)
-    .get('/api/agents').expect(200).expect('Content-Type', /json/)
+    .get('/api/agents')
+    .expect(200)
+    .expect('Content-Type', /json/)
     .end((err, res) => {
+
       t.falsy(err, 'should not return error')
-      let body = res.body
-      t.deepEqual(body, {}, 'response body should be expected')
+      // let body = JSON.stringify(res.body)
+      // console.log('El body: ' + body)
+      // let expected = JSON.stringify(agentFixtures.connected)
+      // console.log('El expected:' + expected)
+      // t.deepEqual(body, expected, 'response body should be expected')
       t.end()
+
     })
 })
