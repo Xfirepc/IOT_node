0055fa8623bbde2c65309b094f8a9c1b2f7f9be4
Xfirepc <vinygfx@gmail.com>
2018-09-09 19:16:58 -0500

    add agent connected event to cli

--- platziverse-cli/platziverse.js
+++ platziverse-cli/platziverse.js
@@ -5,8 +5,13 @@
 
 const blessed = require('blessed')
 const contrib = require('blessed-contrib')
-
+const PlatziverseAgent = require('platziverse-agent')
 const screen = blessed.screen()
+
+
+const agents = new Map()
+const agentMetrics = new Map()
+const agent = new PlatziverseAgent()
 
 const grid = new contrib.grid({
   rows: 1,
@@ -25,8 +30,30 @@ const line = grid.set(0, 1, 1, 3, contrib.line, {
   xPadding: 5
 })
 
+
+agent.on('agent/connected', payload => {
+  const { uuid } = payload.agent
+
+  if(!agents.has(uuid)) { 
+    agents.set(uuid, payload.agent)
+    agentMetrics.set(uuid, {})
+  }
+
+  renderData()
+})
+
+function renderData () {
+  const treeData = {}
+
+  for(let [uuid, val] of agents) {
+    const title = `${val.name} - (${val.pid})`
+  }
+}
+
 screen.key([ 'escape', 'q', 'C-c' ], (ch, key) => {
   process.exit(0)
 })
 
+
+agent.connect()
 screen.render()
