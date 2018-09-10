7d1f82022afc2c5c0084d1ead4bbb7e76df18cab
Xfirepc <vinygfx@gmail.com>
2018-09-09 20:24:09 -0500

    agente de monitoreo add tree data by agent

--- platziverse-cli/platziverse.js
+++ platziverse-cli/platziverse.js
@@ -1,17 +1,19 @@
-#! /usr/bin/env node
+#!/usr/bin/env node
+
 'use strict'
 
 /* eslint new-cap: "off" */
 
 const blessed = require('blessed')
 const contrib = require('blessed-contrib')
-const PlatziverseAgent = require('platziverse-agent')
-const screen = blessed.screen()
 const moment = require('moment')
+const PlatziverseAgent = require('platziverse-agent')
+
+const agent = new PlatziverseAgent()
+const screen = blessed.screen()
 
 const agents = new Map()
 const agentMetrics = new Map()
-const agent = new PlatziverseAgent()
 
 const grid = new contrib.grid({
   rows: 1,
@@ -25,16 +27,15 @@ const tree = grid.set(0, 0, 1, 1, contrib.tree, {
 
 const line = grid.set(0, 1, 1, 3, contrib.line, {
   label: 'Metric',
-  showLeyend: true,
+  showLegend: true,
   minY: 0,
   xPadding: 5
 })
 
-
 agent.on('agent/connected', payload => {
   const { uuid } = payload.agent
 
-  if(!agents.has(uuid)) { 
+  if (!agents.has(uuid)) {
     agents.set(uuid, payload.agent)
     agentMetrics.set(uuid, {})
   }
@@ -42,11 +43,10 @@ agent.on('agent/connected', payload => {
   renderData()
 })
 
-
 agent.on('agent/disconnected', payload => {
   const { uuid } = payload.agent
 
-  if(agents.has(uuid)) { 
+  if (agents.has(uuid)) {
     agents.delete(uuid)
     agentMetrics.delete(uuid)
   }
@@ -54,12 +54,11 @@ agent.on('agent/disconnected', payload => {
   renderData()
 })
 
-
 agent.on('agent/message', payload => {
   const { uuid } = payload.agent
   const { timestamp } = payload
 
-  if(!agents.has(uuid)) { 
+  if (!agents.has(uuid)) {
     agents.set(uuid, payload.agent)
     agentMetrics.set(uuid, {})
   }
@@ -69,13 +68,13 @@ agent.on('agent/message', payload => {
   payload.metrics.forEach(m => {
     const { type, value } = m
 
-    if(!Array.isArray(metrics[type])) {
+    if (!Array.isArray(metrics[type])) {
       metrics[type] = []
     }
 
     const length = metrics[type].length
-    if(length >= 20) {
-      metrics[type].shitf()
+    if (length >= 20) {
+      metrics[type].shift()
     }
 
     metrics[type].push({
@@ -83,26 +82,38 @@ agent.on('agent/message', payload => {
       timestamp: moment(timestamp).format('HH:mm:ss')
     })
   })
+
   renderData()
 })
 
 function renderData () {
   const treeData = {}
 
-  for(let [uuid, val] of agents) {
-    const title = `${val.name} - (${val.pid})`
+  for (let [ uuid, val ] of agents) {
+    const title = `${val.name}>(${val.pid})`
     treeData[title] = {
       uuid,
       agent: true,
       children: {}
     }
+
+    const metrics = agentMetrics.get(uuid)
+    Object.keys(metrics).forEach(type => {
+      const metric = {
+        uuid,
+        type,
+        metric: true
+      }
+
+      const metricName = ` ${type}`
+      treeData[title].children[metricName] = metric
+    })
   }
 
   tree.setData({
     extended: true,
     children: treeData
   })
-
   screen.render()
 }
 
@@ -110,6 +121,6 @@ screen.key([ 'escape', 'q', 'C-c' ], (ch, key) => {
   process.exit(0)
 })
 
-
 agent.connect()
+tree.focus()
 screen.render()
